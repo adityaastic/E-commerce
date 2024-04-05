2db2508bf484d58ec551414942795adcee57634c
adityaastic <adityaastic@gmail.com>
2024-04-05 19:35:54 +0530

    first commit

---
+++ src/customer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const HomeSectionCard = () => {
+return (
+<div className='cursor-pointer flex flex-col items-center bg-white rounded-1g shadow-lg
+overflow-hidden w-[15rem] mx-3'>
+
+<div className='h-[13rem] w-[10rem]'>
+    <img className='object-cover object-top w-full h-full'src="https://imgs.search.brave.com/yaryA8aJypeVQtDcb1iG3iu5rkh8jifiMtqEQluGqIk/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NzFBZkk0R0JkZkwu/anBn" alt=""/>
+        </div>
+
+        <div className='p-4'>
+<h3 className='text-lg font-medium ☐ text-gray-900'>Nofilter</h3>
+<p className='mt-2 text-sm text-gray-500'>Men Solid Pure Cotton Straight Kurta</p>
+</div>
+
+</div>
+
+)
+}
+
+export default HomeSectionCard
