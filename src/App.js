5fc84dc29256c088a0b1161e9a5a86ebf5863317
adityaastic <adityaastic@gmail.com>
2024-04-07 13:05:31 +0530

    product page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import Navigation from './customer/components/Navigation/Navigation';
 import HomePage from './customer/pages/HomePage.jsx/HomePage';
 import Footer from './customer/components/Footer/footer';
+import Product from './customer/components/Product/Product';
+
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
     <div>
-      <HomePage/>
+      {/* <HomePage/> */}
+    <Product/>
     </div>
     <Footer/>
     </div>
